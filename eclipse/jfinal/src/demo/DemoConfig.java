2c822a095931792e28b952530ffc7a2847c00923
wx <119584828@qq.com>
2018-11-30 11:25:04 +0800

    20181130
    springboottest

---
+++ eclipse/jfinal/src/demo/DemoConfig.java
@@ -0,0 +1,31 @@
+package demo;
+
+import com.jfinal.config.Constants;
+import com.jfinal.config.Handlers;
+import com.jfinal.config.Interceptors;
+import com.jfinal.config.JFinalConfig;
+import com.jfinal.config.Plugins;
+import com.jfinal.config.Routes;
+import com.jfinal.template.Engine;
+
+public class DemoConfig extends JFinalConfig {
+	public void configConstant(Constants me) {
+		me.setDevMode(true);
+	}
+
+	public void configRoute(Routes me) {
+		me.add("/hello", HelloController.class);
+	}
+
+	public void configEngine(Engine me) {
+	}
+
+	public void configPlugin(Plugins me) {
+	}
+
+	public void configInterceptor(Interceptors me) {
+	}
+
+	public void configHandler(Handlers me) {
+	}
+}
